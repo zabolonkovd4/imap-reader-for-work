e3cbdc99b5af38033caab794c5ab3bfc4955ab41
Dmitri Zabolonkov <dmitri.zablonkov@yandex.ru>
2023-07-17 15:14:36 +0300

    simple echo tg client

--- src/telegram-client.py
+++ src/telegram-client.py
@@ -1,4 +1,26 @@
+import logging
+
 from aiogram import Bot, Dispatcher, executor, types
+import aiohttp
+
+API_TOKEN = 'TOKEN'
+PROXY_URL = 'http://PROXY_URL'  # Or 'socks5://host:port'
+PROXY_AUTH = aiohttp.BasicAuth(login='login', password='password')
+
+bot = Bot(token=API_TOKEN, proxy=PROXY_URL, proxy_auth=PROXY_AUTH)
+dp = Dispatcher(bot)
+
+logging.basicConfig(level=logging.INFO)
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Hello, I am nifti task notifier")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
 
 if __name__ == '__main__':
-    pass
+    executor.start_polling(dp, skip_updates=True)
